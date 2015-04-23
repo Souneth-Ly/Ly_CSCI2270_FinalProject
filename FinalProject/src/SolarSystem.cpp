b64826144a0b8e05dabf98c649437b9b2a2c9e5b
user <jule3935@colorado.edu>
2015-04-23 14:07:10 -0600

    FinalProject/

--- FinalProject/src/SolarSystem.cpp
+++ FinalProject/src/SolarSystem.cpp
@@ -1,6 +1,7 @@
 #include "SolarSystem.h"
 #include <iostream>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
@@ -15,7 +16,51 @@ SolarSystem::~SolarSystem()
 }
 
 void SolarSystem::buildSystem(){
+    planetNode *cursor=NULL;
+    string line;
+    ifstream inputfile;
+    inputfile.open("SolarSystemData.txt");
+    if (!inputfile)
+    {
+        cout << "Could not open file\n";
+        return;
+    }
+    string planetName;
+    string color;
+    string diameter;
+    string type;
+    string yearLength;
+    string distFromSun;
+    string temp;
 
+    string inLine;
+    while (getline(inputfile, line))
+    {
+        istringstream inLine(line);
+        getline(inLine, planetName, ',');
+        getline(inLine, color, ',');
+        getline(inLine, diameter, ',');
+        int diam= stoi(diameter);
+        getline(inLine, type, ',');
+        getline(inLine, yearLength, ',');
+        int year= stoi(yearLength);
+        getline(inLine, distFromSun, ',');
+        int distSun= stoi(distFromSun);
+
+        planetNode *newNode= new planetNode(planetName, color, diam, type, year, distSun);
+        if(head==NULL)
+        {
+            head=newNode;
+            cursor=head;
+            cursor->previous=NULL;
+
+        }else{
+            cursor->next=newNode;
+            cursor->previous=cursor;
+            cursor=newNode;
+
+        }
+    }
 }
 
 int SolarSystem::randomFact(int x){
